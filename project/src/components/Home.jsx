d4784d183c6695da7c742bd0d2331e0976dbb0a2
anand <anandkumarch2003@gmail.com>
2025-02-08 08:07:21 +0530

    home

--- project/src/components/Home.jsx
+++ project/src/components/Home.jsx
@@ -2,10 +2,10 @@
 import {Link} from 'react-router-dom'
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold mb-4">Welcome to Smart To-Do List</h1>
+    <div className="p-3 flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl sm:text-4xl font-bold">Welcome to Smart To-Do List</h1>
       <p className="text-lg mb-6">Organize your tasks efficiently!</p>
-      <Link to="/signup" className="bg-blue-500 text-white px-6 py-3 rounded-lg">Get Started</Link>
+      <Link to="/signup" className="bg-blue-500 text-white px-4 py-2 sm:px-6 sm:py-3 rounded-lg">Get Started</Link>
     </div>
   );
 };
